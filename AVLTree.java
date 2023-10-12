ee28de87ec8aeb8ce3ed93424e3b5eb0867a4f9a
GnExia2 <ngai.garrick.j@gmail.com>
2023-10-12 15:12:24 +0800

    completed insert, delete, search, rotation features for AVLTree

--- AVLTree.java
+++ AVLTree.java
@@ -1,16 +1,76 @@
+class TreeNode {
+    String name;
+    TreeNode left;
+    TreeNode right;
+    int height;
+
+    public TreeNode(String name) {
+        this.name = name;
+        this.height = 1;
+    }
+}
+
+
 public class AVLTree {
     private TreeNode root;
 
-    // Rotation methods (leftRotate and rightRotate)
-    private TreeNode leftRotate(TreeNode y) {
-        // Implementation of leftRotate
+    // Get the height of a node
+    private int height(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return node.height;
     }
 
-    private TreeNode rightRotate(TreeNode x) {
-        // Implementation of rightRotate
+    // Get the balance factor of a node
+    private int getBalanceFactor(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return height(node.left) - height(node.right);
     }
 
-    // Node insertion and search methods
+    // Perform a right rotation
+    private TreeNode rightRotate(TreeNode y) {
+        TreeNode x = y.left;
+        TreeNode T2 = x.right;
+
+        x.right = y;
+        y.left = T2;
+
+        y.height = Math.max(height(y.left), height(y.right)) + 1;
+        x.height = Math.max(height(x.left), height(x.right)) + 1;
+
+        return x;
+    }
+
+    // Perform a left rotation
+    private TreeNode leftRotate(TreeNode x) {
+        TreeNode y = x.right;
+        TreeNode T2 = y.left;
+
+        y.left = x;
+        x.right = T2;
+
+        x.height = Math.max(height(x.left), height(x.right)) + 1;
+        y.height = Math.max(height(y.left), height(y.right)) + 1;
+
+        return y;
+    }
+
+    // Perform a leftright rotation
+    private TreeNode leftrightRotate(TreeNode node) {
+        node.left = leftRotate(node.left);
+        return rightRotate(node);
+    }
+
+    // Perform a rightleft rotation
+    private TreeNode rightleftRotate(TreeNode node) {
+        node.right = rightRotate(node.right);
+        return leftRotate(node);
+    }
+
+    // Insert a name into the AVL tree
     public TreeNode insert(TreeNode node, String name) {
         if (node == null) {
             return new TreeNode(name);
@@ -58,6 +118,7 @@ public class AVLTree {
         return node;
     }
 
+    // Search for a name in the AVL tree
     public TreeNode search(TreeNode node, String name) {
         if (node == null || name.equals(node.name)) {
             return node;
@@ -71,5 +132,113 @@ public class AVLTree {
 
         return search(node.right, name);
     }
-    
+
+    // Delete a name from the AVL tree
+    public TreeNode delete(TreeNode node, String name) {
+        if (node == null) {
+            return node;
+        }
+
+        int compareResult = name.compareTo(node.name);
+
+        if (compareResult < 0) {
+            node.left = delete(node.left, name);
+        } else if (compareResult > 0) {
+            node.right = delete(node.right, name);
+        } else {
+            if ((node.left == null) || (node.right == null)) {
+                TreeNode temp = null;
+                if (node.left != null) {
+                    temp = node.left;
+                } else {
+                    temp = node.right;
+                }
+
+                if (temp == null) {
+                    temp = node;
+                    node = null;
+                } else {
+                    node = temp;
+                }
+            } else {
+                TreeNode temp = findMin(node.right);
+                node.name = temp.name;
+                node.right = delete(node.right, temp.name);
+            }
+        }
+
+        if (node == null) {
+            return node;
+        }
+
+        node.height = 1 + Math.max(height(node.left), height(node.right));
+
+        int balance = getBalanceFactor(node);
+
+        if (balance > 1 && getBalanceFactor(node.left) >= 0) {
+            return rightRotate(node);
+        }
+
+        if (balance > 1 && getBalanceFactor(node.left) < 0) {
+            node.left = leftRotate(node.left);
+            return rightRotate(node);
+        }
+
+        if (balance < -1 && getBalanceFactor(node.right) <= 0) {
+            return leftRotate(node);
+        }
+
+        if (balance < -1 && getBalanceFactor(node.right) > 0) {
+            node.right = rightRotate(node.right);
+            return leftRotate(node);
+        }
+
+        return node;
+    }
+
+    // Find the minimum node in a subtree
+    private TreeNode findMin(TreeNode node) {
+        TreeNode current = node;
+        while (current.left != null) {
+            current = current.left;
+        }
+        return current;
+    }
+
+    // In-order traversal of the AVL tree
+    public void inOrderTraversal(TreeNode node) {
+        if (node != null) {
+            inOrderTraversal(node.left);
+            System.out.print(node.name + " ");
+            inOrderTraversal(node.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        AVLTree avlTree = new AVLTree();
+
+        avlTree.root = avlTree.insert(avlTree.root, "Alice");
+        avlTree.root = avlTree.insert(avlTree.root, "Bob");
+        avlTree.root = avlTree.insert(avlTree.root, "Charlie");
+        avlTree.root = avlTree.insert(avlTree.root, "David");
+        avlTree.root = avlTree.insert(avlTree.root, "Eve");
+        avlTree.root = avlTree.insert(avlTree.root, "Frank");
+
+        System.out.println("In-order traversal of AVL tree:");
+        avlTree.inOrderTraversal(avlTree.root); // Should print the sorted order of inserted names
+
+        String nameToSearch = "Charlie";
+        TreeNode result = avlTree.search(avlTree.root, nameToSearch);
+        if (result != null) {
+            System.out.println("\nFound name: " + nameToSearch);
+        } else {
+            System.out.println("\nName " + nameToSearch + " not found");
+        }
+
+        System.out.println("Deleting David from the tree.");
+        avlTree.root = avlTree.delete(avlTree.root, "David");
+
+        System.out.println("In-order traversal of AVL tree after deletion:");
+        avlTree.inOrderTraversal(avlTree.root); // Should print the sorted order after the deletion
+    }
 }
